bbfdfcfc808bd13e0b91daa647e15951a3590fbb
Tran Quoc Cuong <cuongtran.241004@gmail.com>
2025-06-07 11:46:26 +0700

    Add(health-profile): add interface for repo & service

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/Interfaces/IServices/IHealthProfileService.cs
@@ -0,0 +1,24 @@
+﻿using SchoolMedicalSystem.Application.DTO.Request;
+using SchoolMedicalSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.Interfaces.IServices
+{
+    public interface IHealthProfileService
+    {
+        Task<List<HealthProfile>> GetAllAsync();
+        Task<HealthProfile?> GetByIdAsync(int id);
+
+        Task<HealthProfile?> GetByStudentNameAsync(string studentName);
+
+        Task<HealthProfileDTORequest> AddAsync(HealthProfileDTORequest healthProfile);
+
+        Task<bool> UpdateAsync(HealthProfile entity);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
