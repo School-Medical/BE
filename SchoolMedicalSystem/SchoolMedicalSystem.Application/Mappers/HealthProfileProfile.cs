54a4b329dda6789d2c468bae9d0b05c58c85efb1
Tran Quoc Cuong <cuongtran.241004@gmail.com>
2025-06-09 05:50:54 +0700

    Add(health-profile): add mapper

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/Mappers/HealthProfileProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using SchoolMedicalSystem.Application.DTO.Request;
+using SchoolMedicalSystem.Application.DTO.Response;
+using SchoolMedicalSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.Mappers
+{
+    public class HealthProfileProfile : Profile
+    {
+        public HealthProfileProfile()
+        {
+            CreateMap<HealthProfile, HealthProfileDTOResponse>()
+                .ForMember(dest => dest.student_name, opt => opt.MapFrom(src => (src.student.first_name + " " + src.student.last_name)));
+
+            CreateMap<HealthProfileDTORequest, HealthProfile>();
+        }
+    }
+}
