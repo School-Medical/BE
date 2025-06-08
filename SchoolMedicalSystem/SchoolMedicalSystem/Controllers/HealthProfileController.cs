c5013e882001cbcfaa4b90b33bfd84951834b4f8
Tran Quoc Cuong <cuongtran.241004@gmail.com>
2025-06-09 05:51:46 +0700

    Add(health-profile): create health-profile controller

---
+++ SchoolMedicalSystem/SchoolMedicalSystem/Controllers/HealthProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolMedicalSystem.Application.DTO.Response;
+using SchoolMedicalSystem.Application.Interfaces.IServices;
+using SchoolMedicalSystem.Domain.Entities;
+using SchoolMedicalSystem.Models;
+
+namespace SchoolMedicalSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthProfileController : ControllerBase
+    {
+        private readonly IHealthProfileService _healthProfileService;
+
+        public HealthProfileController(IHealthProfileService healthProfileService)
+        {
+            _healthProfileService = healthProfileService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllHealthProfiles()
+        {
+            try
+            {
+                var healthProfiles = await _healthProfileService.GetAllAsync();
+                return Ok(new ApiResponse<List<HealthProfileDTOResponse>>("Health profiles retrieved successfully.", healthProfiles, 200));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex);
+            }
+        }
+    }
+}
