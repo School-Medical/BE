dba51b38fb3e7c8b111ca6c85970dcbaa8612fb0
The-Trungg <trungntse183179@fpt.edu.vn>
2025-06-10 08:37:18 +0700

    Add: setup base for JWT

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/DTO/Response/UserLoginDTOResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.DTO.Response
+{
+    public class UserLoginDTOResponse
+    {
+        public class UserLoginResponseDto
+        {
+            public int UserId { get; set; }
+            public string Account { get; set; }
+            public string? FirstName { get; set; }
+            public string? LastName { get; set; }
+            public int? RoleId { get; set; }
+            public string Token { get; set; } = null!;
+        }
+
+    }
+}
---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolMedicalSystem.Application.Interfaces.IReposervices;
+using SchoolMedicalSystem.Domain.Entities;
+using SchoolMedicalSystem.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SchoolMedicalDbContext _dbContext;
+        public UserRepository(SchoolMedicalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User?> GetByAccountAsync(string account)
+        {
+            return await _dbContext.Users.Include(u => u.role)
+                                         .FirstOrDefaultAsync(u => u.account.Equals(account));
+        }
+    }
+}
