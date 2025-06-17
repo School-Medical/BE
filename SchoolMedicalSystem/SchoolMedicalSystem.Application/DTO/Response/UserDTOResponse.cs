003a2f1f93d68c5068dc9bbe89ab9ab1fbcf3db0
Tran Quoc Cuong <cuongtran.241004@gmail.com>
2025-06-18 06:14:23 +0700

    Add(user): create dto

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/DTO/Response/UserDTOResponse.cs
@@ -0,0 +1,31 @@
+﻿using SchoolMedicalSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.DTO.Response
+{
+    public class UserDTOResponse
+    {
+        public int UserId { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Account { get; set; }
+
+        public string? RoleName { get; set; }
+
+        public virtual ICollection<StudentParent> StudentParents { get; set; } = new List<StudentParent>();
+
+        public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+
+    }
+}
