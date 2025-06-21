79557cacf13eb0062758a44f8fe1263a16dcda18
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-06-21 10:22:08 +0700

    Add(student): create interface

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/Interfaces/IServices/IStudentService.cs
@@ -0,0 +1,22 @@
+﻿using SchoolMedicalSystem.Application.DTO.Request;
+using SchoolMedicalSystem.Application.DTO.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.Interfaces.IServices
+{
+    public interface IStudentService
+    {
+        Task<StudentDTOResponse> GetStudentByIdAsync(int id);
+        Task<StudentDTOResponse> GetStudentByStudentCodeAsync(string studentCode);
+        Task<List<StudentDTOResponse>> GetStudentByStudentNameAsync(string studentName);
+        Task<List<StudentDTOResponse>> GetStudentsByClassIdAsync(int classId);
+        Task<PaginatedResponse<StudentDTOResponse>> GetAllStudentsAsync(int pageSize, int pageNumber);
+        Task<StudentDTOResponse> AddStudentAsync(StudentDTORequest student);
+        Task<StudentDTOResponse> UpdateStudentAsync(StudentDTORequest student);
+        Task<List<StudentDTOResponse>> AddStudentsToClassAsync(List<StudentDTORequest> students, int classId);
+    }
+}
