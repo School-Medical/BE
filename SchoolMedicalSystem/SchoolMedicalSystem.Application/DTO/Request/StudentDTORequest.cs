438d930ca31f9abeb5128650fa61d1b44c394117
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2025-06-21 10:20:51 +0700

    Add(student): create dto

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/DTO/Request/StudentDTORequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.DTO.Request
+{
+    public class StudentDTORequest
+    {
+        public string? StudentCode { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public int? ClassId { get; set; }
+    }
+}
