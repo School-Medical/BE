8f6088b7876407c7c9dd2c5659c02470bd632788
The-Trungg <trungntse183179@fpt.edu.vn>
2025-07-01 16:47:59 +0700

    Add: PrescriptionMedicine DTO and mapper

---
+++ SchoolMedicalSystem/SchoolMedicalSystem.Application/DTO/Request/PrescriptionMedicineDTORequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedicalSystem.Application.DTO.Request
+{
+    public class PrescriptionMedicineDTORequest
+    {
+        public int PrescriptionMedicineId { get; set; }
+        public int? PescriptionId { get; set; }
+        public int? MedicineId { get; set; }
+        public int? Quantity { get; set; }
+    }
+}
